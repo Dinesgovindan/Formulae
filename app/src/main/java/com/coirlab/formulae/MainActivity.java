fc99599dd27ddac4f0acb65203b2dc022c677dc6
Dinesgovindan <dinesgovindan@gmail.com>
2017-03-28 00:20:14 +0530

    Java initial changes

--- app/src/main/java/com/coirlab/formulae/MainActivity.java
+++ app/src/main/java/com/coirlab/formulae/MainActivity.java
@@ -2,12 +2,33 @@ package com.coirlab.formulae;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.formula_name_list);
+    }
+
+    public void populate_frequency(View freq_view) {
+        setContentView(R.layout.single_formula_view);
+    }
+
+    public void populate_voltage(View volt_view) {
+        setContentView(R.layout.single_formula_view);
+    }
+
+    public void populate_power(View power_view) {
+        setContentView(R.layout.single_formula_view);
+    }
+
+    public void populate_power_volt(View power_volt_view) {
+        setContentView(R.layout.single_formula_view);
+    }
+
+    public void populate_power_current(View power_current_view) {
+        setContentView(R.layout.single_formula_view);
     }
 }
