935135b8264c32520b01f3ca68c2ee33b5afd0d2
Dinesgovindan <dinesgovindan@gmail.com>
2017-04-01 21:59:42 +0530

    Changes for more than one activity for mutiple screens

---
+++ app/src/main/java/com/coirlab/formulae/FormulaActivity.java
@@ -0,0 +1,62 @@
+package com.coirlab.formulae;
+
+import android.os.Bundle;
+import android.support.annotation.ArrayRes;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+/**
+ * Created by Dinesh on 01-04-2017.
+ */
+public class FormulaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v("Dinesh child activity", "");
+        //setContentView(R.layout.single_formula_view);
+        @StringRes int str_id = getIntent().getExtras().getInt("id");
+
+        String formula;
+        Spinner formula_fields;
+        TextView formula_view;
+        @ArrayRes int array_id=0;
+
+        setContentView(R.layout.single_formula_view);
+        switch(str_id) {
+            case R.string.frequency:
+                array_id = R.array.frequency;
+                break;
+            case R.string.voltage:
+                array_id = R.array.voltage;
+                break;
+            case R.string.power:
+                array_id = R.array.power;
+                break;
+            case R.string.power_voltage:
+                array_id = R.array.power_voltage;
+                break;
+            case R.string.power_current:
+                array_id = R.array.power_current;
+                break;
+            default:
+                //logs
+                break;
+        }
+
+        //set formula drop down
+        formula_fields = (Spinner)findViewById(R.id.formula_fields);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                array_id, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        formula_fields.setAdapter(adapter);
+
+        //Set Formula
+        formula = getString(str_id);
+        formula_view = (TextView) findViewById(R.id.single_formula_view);
+        formula_view.setText(formula);
+    }
+}
--- app/src/main/java/com/coirlab/formulae/MainActivity.java
+++ app/src/main/java/com/coirlab/formulae/MainActivity.java
@@ -1,8 +1,16 @@
 package com.coirlab.formulae;
 
+import android.content.Intent;
+import android.support.annotation.ArrayRes;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,23 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.formula_name_list);
     }
 
+    public  void populate_single_formula(@StringRes int string_id) {
+        Intent intent = new Intent(this, FormulaActivity.class);
+        intent.putExtra("id", string_id);
+        startActivity(intent);
+
+    }
     public void populate_frequency(View freq_view) {
-        setContentView(R.layout.single_formula_view);
+        populate_single_formula(R.string.frequency);
     }
 
     public void populate_voltage(View volt_view) {
-        setContentView(R.layout.single_formula_view);
+        populate_single_formula(R.string.voltage);
     }
 
-    public void populate_power(View power_view) {
-        setContentView(R.layout.single_formula_view);
+    public void populate_power(View power_view) {;
+        populate_single_formula(R.string.power);
     }
 
     public void populate_power_volt(View power_volt_view) {
-        setContentView(R.layout.single_formula_view);
+        populate_single_formula(R.string.power_voltage);
     }
 
     public void populate_power_current(View power_current_view) {
-        setContentView(R.layout.single_formula_view);
+        populate_single_formula(R.string.power_current);
     }
 }
